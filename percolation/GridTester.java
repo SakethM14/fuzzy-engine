2b78ce028ffee061cda58de92cf9645171039a36
Saketh <saketh_malyala@yahoo.com>
2021-02-18 00:36:12 -0800

    realized i didnt us class in gridtester

--- percolation/GridTester.java
+++ percolation/GridTester.java
@@ -1,10 +1,11 @@
 package percolation;
 
-public GridTester{
+public class GridTester{
 
     public static void main(String[] args){
 
         Grid g = new Grid(4, 4, 0.5);
         System.out.println(G);
     }
+    
 }
