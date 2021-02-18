3f8ab16a1bf559681b14be21ce76505794b4ff64
Saketh <saketh_malyala@yahoo.com>
2021-02-18 00:15:53 -0800

    working on grid

---
+++ percolation/Grid.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+public class Grid{
+
+    int[][] cells;
+    Node[][] nodeCells;
+    int[] dim;
+    double p;
+
+    public Grid(int length, int width, double probability) {
+        cells = new int[length][width];
+        dim = {length, width};
+        p = probability;
+        fill(p);
+        nodeCells = new Node[length][width];
+
+    }
+    public void fill(){
+        Random r = new Random();
+        for (int i = 0; i < dim[0]; i++) {
+            for (int j = 0; j < dim[1]; j++) {
+                if (nextDouble() < p){
+                    cells[i][j] = 1;
+                }
+                else {
+                    cells[i][j] = 0;
+                }
+            }
+        }
+    }
+    public Node[][] toNode(){
+        for (int i = 0; i < dim[0]; i++) {
+            for (int j = 0; j < dim[1]; j++) {
+                nodeCells[i][j] = new Node(cells[i][j]);
+            }
+        }
+        for (int i = 0; i < dim[0]; i++) {
+            for (int j = 0; j < dim[1]; j++) {
+                if (j < dim[1]-1){
+                    nodeCells[i][j].neighbors.add(nodeCells[i][j+1]);
+                    nodeCells[i][j+1].neighbors.add(nodeCells[i][j]);
+                }
+                if (i < dim[1]-1){
+                    nodeCells[i][j].neighbors.add(nodeCells[i+1][j]);
+                    nodeCells[i+1][j].neighbors.add(nodeCells[i][j]);
+                }
+                
+            }
+        }
+
+
+    }
+}
--- percolation/Node.java
+++ percolation/Node.java
@@ -1,5 +1,28 @@
 package percolation;
 
+public class Node{
 
+    int status;
+    boolean checked;
+    // static final NUM_NEIGHBORS = 4;
+    Set<Node> neighbors;
+    boolean top;
+    boolean bottom;
+    public Node(int status){
+        this.status = status;
+        this.checked = false;
+        this.neighbors = new HashSet<Node>();
+        this.top = false;
+        this.bottom = false;
+    }
+    public void top(){
+        top = true;
+    }
+    public void bottom(){
+        bottom = true;
+    }
+
+
+}
 
 
